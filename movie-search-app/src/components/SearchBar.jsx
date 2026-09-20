b61864f616707e1119a27245f3b2332a7e86659e
Oswaldo Cabrera <124094692+tacomcfancy@users.noreply.github.com>
2026-09-19 21:33:24 -0700

    started on the project

---
+++ movie-search-app/src/components/SearchBar.jsx
@@ -0,0 +1,16 @@
+function SearchBar() {
+    return (
+        <div>
+            <input 
+                type="text"
+                placeholder="Search for movies..."
+            />
+
+            <button>
+                Search
+            </button>
+        </div>
+    );
+}
+
+export default SearchBar;
